fedfc3fa2ef174adccac830d8a398e4fad6a8dc3
Egidijus Lileika <lileikaa@gmail.com>
2024-04-17 20:53:09 +0300

    Minimal working framework

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //
 // RSFX is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
+// the Free Software Foundation, either version 2.1 of the License, or
 // (at your option) any later version.
 //
 // RSFX is distributed in the hope that it will be useful,
@@ -27,10 +27,80 @@ mod mesh;
 mod texture;
 mod gl_renderer;
 mod byte_buffer_reader;
-pub mod game_context;
 pub mod game_status;
 pub mod scene;
 pub mod color;
 pub mod input;
 pub mod renderer;
 pub mod rsfx_context;
+
+use crate::scene::Scene;
+use crate::game_status::GameStatus;
+use crate::rsfx_context::RsfxContext;
+
+pub struct Rsfx;
+
+impl Rsfx {
+    const TICK_RATE: u128 = (1.0f64 / 60.0f64 * 1000.0f64) as u128;
+
+    pub fn new() -> Rsfx {
+        Rsfx { }
+    }
+
+    // This func is mutable to ensure that this object is not used more than once when game is running
+    pub fn run(&mut self, game_name: &str, starting_scene: Box<dyn Scene>) -> Result<(), String> {
+        let mut rsfx_context = RsfxContext::new(game_name)?;
+
+        let mut current_scene = starting_scene;
+
+        current_scene.on_start(&mut rsfx_context.get_renderer_mut());
+
+        let delta_time = RsfxContext::time_now();
+        let mut last_frame_time = delta_time;
+
+        let mut game_status = GameStatus::new();
+        'main_loop: loop {
+            let input = rsfx_context.poll_input_events();
+            if input.should_quit() {
+                break 'main_loop;
+            }
+            
+            rsfx_context.begin_rendering();
+
+            let time_now = RsfxContext::time_now();
+            if time_now >= last_frame_time + Rsfx::TICK_RATE {
+                let delta_time = time_now - last_frame_time;
+                last_frame_time = time_now;
+
+                // Update scene
+                match current_scene.on_update(
+                    &mut game_status,
+                    rsfx_context.get_renderer_mut(),
+                    &input,
+                    delta_time as f64 / 1000.0
+                ) {
+                    Some(scene) => {
+                        current_scene.on_destroy();
+                        current_scene = scene;
+
+                        let renderer = rsfx_context.get_renderer_mut();
+
+                        // TODO: Reset renderer if needed
+
+                        current_scene.on_start(renderer);
+                    }
+                    _ => {
+                        if game_status.should_quit() {
+                            break 'main_loop
+                        }
+                    }
+                };
+            }
+            
+            rsfx_context.end_rendering();
+            rsfx_context.swap_buffer();
+        }
+
+        Ok(())
+    }
+}
--- src/scene.rs
+++ src/scene.rs
@@ -5,7 +5,7 @@
 //
 // RSFX is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
+// the Free Software Foundation, either version 2.1 of the License, or
 // (at your option) any later version.
 //
 // RSFX is distributed in the hope that it will be useful,
@@ -22,7 +22,6 @@ use crate::renderer::Renderer;
 use crate::game_status::GameStatus;
 
 pub trait Scene {
-    // It is useful to pass renderer on_start to do basic setup such as background color or set palette
     fn on_start(&mut self, renderer: &mut Renderer);
 
     fn on_update(&mut self, game_status: &mut GameStatus, renderer: &mut Renderer, input: &Input, delta_time: f64) -> Option<Box<dyn Scene>>;
